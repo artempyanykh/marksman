5b283d370a02e71a4b7453ef730b5da52f6c8453
Artem Pyanykh <artem.pyanykh@gmail.com>
2022-05-18 22:50:00 +0100

    misc: Add wait-for-debugger option for LSP server
    When issues happen during server initialization it can be very hard to pin-point what's wrong. This option gives time to connect to the newly started process with a debugger which significantly simplifies troubleshooting.

--- Marksman/Program.fs
+++ Marksman/Program.fs
@@ -1,6 +1,8 @@
 ﻿module Marksman.Program
 
 open System
+open System.Diagnostics
+open System.Threading
 open Ionide.LanguageServerProtocol
 open Ionide.LanguageServerProtocol.Logging
 open Serilog
@@ -33,23 +35,29 @@ let configureLogging (verbosity: int) : unit =
 
     ()
 
-let startLSP (verbosity: int) : int =
+let startLSP (args: int * bool) : int =
+    let verbosity, waitForDebugger = args
+
     use input = Console.OpenStandardInput()
     use output = Console.OpenStandardOutput()
 
     configureLogging verbosity
+    let logger = LogProvider.getLoggerByName "LSP Entry"
 
-    let logger =
-        LogProvider.getLoggerByName "LSP Entry"
+    if waitForDebugger && not Debugger.IsAttached then
+        logger.warn (Log.setMessage "Waiting for debugger to attach...")
+
+        while not Debugger.IsAttached do
+            Thread.Sleep(1000)
+
 
     logger.info (Log.setMessage "Starting Marksman LSP server")
 
-    let requestHandlings =
-        Server.defaultRequestHandlings ()
+    let requestHandlings = Server.defaultRequestHandlings ()
 
     let result =
         Server.start requestHandlings input output MS.MarksmanClient (fun client -> new MS.MarksmanServer(client))
-    
+
     logger.trace (Log.setMessage "Stopped Marksman LSP server")
 
     int result
@@ -58,16 +66,19 @@ let startLSP (verbosity: int) : int =
 let main args =
     let verbosity =
         Input.Option([ "--verbose"; "-v" ], 2, "Set logging verbosity level")
-        
+
+    let waitForDebugger =
+        Input.Option([ "--wait-for-debugger" ], false, "Wait for debugger to attach before running the program")
+
     let lspCommand =
         command "server" {
             description "Start LSP server on stdin/stdout"
-            inputs verbosity
+            inputs (verbosity, waitForDebugger)
             setHandler startLSP
         }
-        
+
     rootCommand args {
         description "Marksman is a language server for Markdown"
-        setHandler (fun () -> startLSP 2)
+        setHandler (fun () -> startLSP (2, false))
         addCommand lspCommand
     }

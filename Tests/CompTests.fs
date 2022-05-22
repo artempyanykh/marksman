32097aa876138f684588091e1287bfe562ef4a4f
Artem Pyanykh <artem.pyanykh@gmail.com>
2022-05-22 12:41:11 +0100

    core: Add shortcut link para-element in completion

--- Tests/CompTests.fs
+++ Tests/CompTests.fs
@@ -7,8 +7,9 @@ open Marksman.Comp
 open Marksman.Misc
 
 let mkTitleComp range = Comp.Comp.Title(range, true)
+let mkShortcutComp range = Comp.Comp.Shortcut(range, true)
 
-module CompOfText =
+module WikiOfText =
     [<Fact>]
     let empty () =
         let text = Text.mkText ""
@@ -16,36 +17,78 @@ module CompOfText =
         Assert.Equal(None, comp)
 
     [<Fact>]
-    let wikiEmptyEol () =
+    let emptyEol () =
         let text = Text.mkText "[["
         let comp = compOfText (Position.Mk(0, 2)) text
         let expected = mkTitleComp (Range.Mk(0, 2, 1, 0))
         Assert.Equal(Some expected, comp)
 
     [<Fact>]
-    let wikiEmptyNonEol () =
+    let emptyNonEol () =
         let text = Text.mkText "[[ "
         let comp = compOfText (Position.Mk(0, 2)) text
         let expected = mkTitleComp (Range.Mk(0, 2, 0, 2))
         Assert.Equal(Some expected, comp)
 
     [<Fact>]
-    let wikiSomeEol () =
+    let someEol () =
         let text = Text.mkText "[[t"
         let comp = compOfText (Position.Mk(0, 2)) text
         let expected = mkTitleComp (Range.Mk(0, 2, 1, 0))
         Assert.Equal(Some expected, comp)
 
     [<Fact>]
-    let wikiSomeWs () =
+    let someWs () =
         let text = Text.mkText "[[t "
         let comp = compOfText (Position.Mk(0, 2)) text
         let expected = mkTitleComp (Range.Mk(0, 2, 0, 3))
         Assert.Equal(Some expected, comp)
 
     [<Fact>]
-    let wikiSomeAndTextAfter () =
+    let someAndTextAfter () =
         let text = Text.mkText "[[t other"
         let comp = compOfText (Position.Mk(0, 2)) text
         let expected = mkTitleComp (Range.Mk(0, 2, 0, 3))
+        Assert.Equal(Some expected, comp)
+
+module LinkOfText =
+    [<Fact>]
+    let empty () =
+        let text = Text.mkText ""
+        let comp = compOfText (Position.Mk(0, 0)) text
+        Assert.Equal(None, comp)
+
+    [<Fact>]
+    let emptyEol () =
+        let text = Text.mkText "["
+        let comp = compOfText (Position.Mk(0, 1)) text
+        let expected = mkShortcutComp (Range.Mk(0, 1, 1, 0))
+        Assert.Equal(Some expected, comp)
+
+    [<Fact>]
+    let emptyNonEol () =
+        let text = Text.mkText "[ "
+        let comp = compOfText (Position.Mk(0, 1)) text
+        let expected = mkShortcutComp (Range.Mk(0, 1, 0, 1))
+        Assert.Equal(Some expected, comp)
+
+    [<Fact>]
+    let someEol () =
+        let text = Text.mkText "[t"
+        let comp = compOfText (Position.Mk(0, 1)) text
+        let expected = mkShortcutComp (Range.Mk(0, 1, 1, 0))
+        Assert.Equal(Some expected, comp)
+
+    [<Fact>]
+    let someWs () =
+        let text = Text.mkText "[t "
+        let comp = compOfText (Position.Mk(0, 1)) text
+        let expected = mkShortcutComp (Range.Mk(0, 1, 0, 2))
+        Assert.Equal(Some expected, comp)
+
+    [<Fact>]
+    let someAndTextAfter () =
+        let text = Text.mkText "[t other"
+        let comp = compOfText (Position.Mk(0, 1)) text
+        let expected = mkShortcutComp (Range.Mk(0, 1, 0, 2))
         Assert.Equal(Some expected, comp)

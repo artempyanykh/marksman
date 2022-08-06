e66bcdc6869e11dc73bb3e70ad9f9cfc535047e2
Artem Pyanykh <artem.pyanykh@gmail.com>
2022-08-06 17:17:01 +0100

    feat: Implement rename refactor for headings
    Renaming headings will fix up all links (both wiki- and markdown inline
    links) that reference the heading.

--- Marksman/Refactor.fs
+++ Marksman/Refactor.fs
@@ -1,5 +1,6 @@
 module Marksman.Refactor
 
+open System
 open Ionide.LanguageServerProtocol.Types
 open Marksman.Workspace
 open Cst
@@ -22,6 +23,9 @@ let isValidLabel (name: string) =
     let badChars = [ "\n"; "["; "]"; "("; ")" ]
     badChars |> List.exists name.Contains |> not
 
+let isValidTitle (name: string) =
+    let badChars = [ "\n"; "#" ]
+    badChars |> List.exists name.Contains |> not
 
 let groupByFirst pairs =
     Seq.groupBy fst pairs
@@ -46,6 +50,36 @@ let renameMarkdownLabel (newLabel: string) (element: Element) : option<TextEdit>
 let renameMarkdownLabelsInDoc newLabel (doc: Doc, els) =
     let edits =
         Seq.collect (renameMarkdownLabel newLabel >> Option.toArray) els
+        |> Array.ofSeq
+
+    let lspDoc = { Uri = doc.path.DocumentUri; Version = doc.version }
+    { TextDocument = lspDoc; Edits = edits }
+
+let renameHeadingLink (heading: Heading) (newSlug: Slug) (element: Element) : option<TextEdit> =
+    match element with
+    | WL { data = wl } ->
+        let toEdit = if Heading.isTitle heading then wl.doc else wl.heading
+
+        toEdit
+        |> Option.map (fun node -> { Range = node.range; NewText = Slug.toString newSlug })
+    | ML { data = MdLink.IL (_, url, _) } ->
+        let docUrl = url |> Option.map DocUrl.ofUrlNode
+
+        let toEdit =
+            if not (Heading.isTitle heading) then
+                docUrl |> Option.bind DocUrl.anchor
+            else
+                None
+
+        toEdit
+        |> Option.map (fun node -> { Range = node.range; NewText = Slug.toString newSlug })
+    | _ -> None
+
+let renameHeadingLinksInDoc heading newTitle (doc: Doc, els) =
+    let newSlug = Slug.ofString newTitle
+
+    let edits =
+        Seq.collect (renameHeadingLink heading newSlug >> Option.toArray) els
         |> Array.ofSeq
 
     let lspDoc = { Uri = doc.path.DocumentUri; Version = doc.version }
@@ -91,6 +125,31 @@ let rename
             Edit workspaceEdit
         else
             Skip
+    | Some (H { data = heading } as el) ->
+        if not (isValidLabel newName) then
+            Error $"Not a valid title: {newName}"
+        else if heading.title.range.ContainsInclusive pos then
+            let headingEdit = { Range = heading.title.range; NewText = newName }
+
+            let refs = Ref.findElementRefs false folder srcDoc el
+            let byDoc = refs |> groupByFirst
+
+            let docEdits =
+                byDoc
+                |> Seq.map (fun (doc, els) ->
+                    let linkEdits = renameHeadingLinksInDoc heading newName (doc, els)
+
+                    if doc = srcDoc then
+                        { linkEdits with Edits = Array.append [| headingEdit |] linkEdits.Edits }
+                    else
+                        linkEdits)
+                |> Array.ofSeq
+
+            let workspaceEdit = mkWorkspaceEdit supportsDocumentEdit docEdits
+
+            Edit workspaceEdit
+        else
+            Skip
     | _ -> Skip
 
 let renameRange (srcDoc: Doc) (pos: Position) : option<Range> =
@@ -103,6 +162,11 @@ let renameRange (srcDoc: Doc) (pos: Position) : option<Range> =
     | Some (MLD { data = def }) ->
         if def.label.range.ContainsInclusive pos then
             Some def.label.range
+        else
+            None
+    | Some (H { data = heading }) ->
+        if heading.title.range.ContainsInclusive pos then
+            Some heading.title.range
         else
             None
     | _ -> None

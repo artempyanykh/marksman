27769be155ceeab2e935acf8666e3ddc7c4afc9f
Artem Pyanykh <artem.pyanykh@gmail.com>
2022-11-24 22:26:58 +0000

    core: Add a config option to configure recognized markdown file extensions

--- Marksman/Config.fs
+++ Marksman/Config.fs
@@ -13,6 +13,40 @@ type LookupError =
 
 type LookupResult<'R> = Result<'R, LookupError>
 
+module LookupResult =
+    let collect (results: list<option<'R>>) : option<array<'R>> =
+        let rec go (acc: list<'R>) (rest: list<option<'R>>) : option<list<'R>> =
+            match rest with
+            | [] -> Some acc
+            | None :: _ -> None
+            | Some r :: tail -> go (r :: acc) tail
+
+        Option.map (List.rev >> Array.ofList) (go [] results)
+
+let rec private tryCoerce<'R> (targetType: System.Type) (value: obj) : option<'R> =
+    if targetType.IsInstanceOfType(value) then
+        Some(value :?> 'R)
+    else
+        match value with
+        | :? TomlArray as value when targetType.IsArray ->
+            let elType = targetType.GetElementType()
+
+            let coercedEls =
+                seq { for el in value -> tryCoerce elType el } |> List.ofSeq
+
+            match LookupResult.collect coercedEls with
+            | None -> None
+            | Some array ->
+                // Apparently, one cannot simply cast an object[] with string elements to string[].
+                // What works is creating an instance of an array with the right dynamic type and
+                // then copying all elements to this new array. This feels wrong! There must be a
+                // better way...
+                let castedArray = System.Array.CreateInstance(elType, array.Length)
+                Array.iteri (fun i v -> castedArray.SetValue(v, i)) array
+                Some(box castedArray :?> 'R)
+        | _ -> None
+
+
 let private getFromTable<'R>
     (table: TomlTable)
     (revContext: list<string>)
@@ -24,9 +58,9 @@ let private getFromTable<'R>
         | [ last ] ->
             match table.TryGetValue(last) with
             | true, value ->
-                match value with
-                | :? 'T as value -> Ok value
-                | _ -> Error(WrongType(List.rev (last :: revContext), value, typeof<'T>))
+                match tryCoerce<'R> typeof<'R> value with
+                | Some value -> Ok value
+                | _ -> Error(WrongType(List.rev (last :: revContext), value, typeof<'R>))
             | false, _ -> Error(NotFound(List.rev (last :: revContext)))
         | next :: tail ->
             match table.TryGetValue(next) with
@@ -54,26 +88,43 @@ let private getFromTableOpt<'R> table revSeenPath remPath : Result<option<'R>, L
 /// Note: all config options are laid out flat to make working with the config
 /// without lenses manageable.
 type Config =
-    { caTocEnable: option<bool> }
+    { caTocEnable: option<bool>
+      coreMarkdownExtensions: option<array<string>> }
 
-    static member Default = { caTocEnable = Some true }
+    static member Default =
+        { caTocEnable = Some true
+          coreMarkdownExtensions = Some [| "md"; "markdown" |] }
+
+    static member Empty = { caTocEnable = None; coreMarkdownExtensions = None }
 
     member this.CaTocEnable() =
         this.caTocEnable
-        |> Option.orElse (Config.Default.caTocEnable)
+        |> Option.orElse Config.Default.caTocEnable
+        |> Option.get
+
+    member this.CoreMarkdownExtensions() =
+        this.coreMarkdownExtensions
+        |> Option.orElse Config.Default.coreMarkdownExtensions
         |> Option.get
 
 let private configOfTable (table: TomlTable) : LookupResult<Config> =
     monad {
         let! caTocEnable = getFromTableOpt<bool> table [] [ "code_action"; "toc"; "enable" ]
 
-        { caTocEnable = caTocEnable }
+        let! coreMarkdownExtensions =
+            getFromTableOpt<array<string>> table [] [ "core"; "markdown"; "extensions" ]
+
+        { caTocEnable = caTocEnable
+          coreMarkdownExtensions = coreMarkdownExtensions }
     }
 
 module Config =
     let logger = LogProvider.getLoggerByName "Config"
 
-    let merge hi low = { caTocEnable = hi.caTocEnable |> Option.orElse low.caTocEnable }
+    let merge hi low =
+        { caTocEnable = hi.caTocEnable |> Option.orElse low.caTocEnable
+          coreMarkdownExtensions =
+            hi.coreMarkdownExtensions |> Option.orElse low.coreMarkdownExtensions }
 
     let tryParse (content: string) =
         let mutable table, diag = null, null

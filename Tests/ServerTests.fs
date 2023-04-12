21498940c23078e6a7c149b6fa132311ca975a28
Artem Pyanykh <artem.pyanykh@gmail.com>
2023-04-12 10:40:12 +0100

    client: Allow clients to specify preferred text sync kind
    Marksman allows users configure text sync kind in their config. With no
    config marksman defaults to Full text sync. Some clients (e.g. ST) would
    like a different default. Now such clients can supply it as part of
    initialization options:
    ```
    {
      "preferredTextSyncKind": 1|2
    }
    ```
    
    This option has lower precedence than whatever user configures in their
    global/workspace-local config file.

---
+++ Tests/ServerTests.fs
@@ -0,0 +1,44 @@
+module Marksman.ServerTests
+
+open Xunit
+
+open Marksman.Config
+open Marksman.State
+open Marksman.Workspace
+open Marksman.Server
+
+open Marksman.Helpers
+
+module ServerUtilTests =
+    [<Fact>]
+    let textSync_UserConfigEmptyWS () =
+        let c = {Config.Default with coreTextSync = Some Incremental }
+        let clientDesc = ClientDescription.empty 
+        let ws = Workspace.ofFolders (Some c) []
+        Assert.Equal(("userConfig", Incremental), ServerUtil.calcTextSync (Some c) ws clientDesc)
+        
+    [<Fact>]
+    let textSync_NoConfigEmptyWS () =
+        let clientDesc = ClientDescription.empty 
+        let ws = Workspace.ofFolders None []
+        Assert.Equal(("default", Full), ServerUtil.calcTextSync None ws clientDesc)
+        
+    [<Fact>]
+    let textSync_NoConfigEmptyWS_PreferIncr () =
+        let clientDesc = {ClientDescription.empty with opts = {preferredTextSyncKind = Some Incremental }}
+        let ws = Workspace.ofFolders None []
+        Assert.Equal(("clientOption", Incremental), ServerUtil.calcTextSync None ws clientDesc)
+        
+    [<Fact>]
+    let textSync_NoConfigNonEmptyWS_PreferIncr () =
+        let clientDesc = {ClientDescription.empty with opts = {preferredTextSyncKind = Some Incremental }}
+        let folder = Folder.multiFile "test" (dummyRootPath ["test"] |> RootPath.ofString) Map.empty None
+        let ws = Workspace.ofFolders None [folder]
+        Assert.Equal(("clientOption", Incremental), ServerUtil.calcTextSync None ws clientDesc)
+        
+    [<Fact>]
+    let textSync_NonEmptyWS_PreferIncrButConfigTakesPrecedence () =
+        let clientDesc = {ClientDescription.empty with opts = {preferredTextSyncKind = Some Incremental }}
+        let folder = Folder.multiFile "test" (dummyRootPath ["test"] |> RootPath.ofString) Map.empty (Some {Config.Empty with coreTextSync = Some Full })
+        let ws = Workspace.ofFolders None [folder]
+        Assert.Equal(("workspaceConfig", Full), ServerUtil.calcTextSync None ws clientDesc)
--- scripts/marksman-server.sh
+++ scripts/marksman-server.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 TOP_DIR="$SCRIPT_DIR/.."
 make -C "$TOP_DIR" build </dev/null 1>&2
 
-exec "$TOP_DIR/Marksman/bin/Debug/net6.0/marksman" "${@:1}" server -v=4
+exec "$TOP_DIR/Marksman/bin/Debug/net7.0/marksman" "${@:1}" server -v=4

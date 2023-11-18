dd5b4f5c798c042cefe861362a124d3d6bc69f7b
TheBlob42 <hessenmobbel@web.de>
2023-11-18 23:48:32 +0100

    test: tags test adaptions
    - fix tags completion tests
    - add tags to document symbol tests
    - implement workspace symbol tests (including tags)

--- Tests/SymbolsTests.fs
+++ Tests/SymbolsTests.fs
@@ -4,14 +4,76 @@ open Xunit
 
 open Ionide.LanguageServerProtocol.Types
 
+open Marksman.Misc
 open Marksman.Helpers
+open Marksman.Workspace
+
+module WorkspaceSymbol =
+    let doc1 =
+        FakeDoc.Mk(path = "doc1.md", contentLines = [| "# A"; "#tag1" |])
+
+    let doc2 =
+        FakeDoc.Mk(path = "doc2.md", contentLines = [| "# B"; "#tag1 #tag2" |])
+
+    let folder = FakeFolder.Mk [ doc1; doc2 ]
+    let workspace = Workspace.ofFolders None [ folder ]
+
+    [<Fact>]
+    let symbols_noQuery =
+        let symbols = Symbols.workspaceSymbols "" workspace
+
+        Assert.Equal(
+            [ { Name = "H1: A"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc1); Range = Range.Mk(0, 0, 0, 3) } }
+              { Name = "Tag: tag1"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc1); Range = Range.Mk(1, 0, 1, 5) } }
+              { Name = "H1: B"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc2); Range = Range.Mk(0, 0, 0, 3) } }
+              { Name = "Tag: tag1"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc2); Range = Range.Mk(1, 0, 1, 5) } }
+              { Name = "Tag: tag2"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc2); Range = Range.Mk(1, 6, 1, 11) } } ],
+            symbols
+        )
+
+    [<Fact>]
+    let symbols_withQuery () =
+        let symbols = Symbols.workspaceSymbols "Tag:" workspace
+
+        Assert.Equal(
+            [ { Name = "Tag: tag1"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc1); Range = Range.Mk(1, 0, 1, 5) } }
+              { Name = "Tag: tag1"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc2); Range = Range.Mk(1, 0, 1, 5) } }
+              { Name = "Tag: tag2"
+                Kind = SymbolKind.String
+                ContainerName = None
+                Location = { Uri = (Doc.uri doc2); Range = Range.Mk(1, 6, 1, 11) } } ],
+            symbols
+        )
 
 module DocSymbols =
     let fakeDoc =
         FakeDoc.Mk(
             [| "# E" //
                "## D"
+               "#t1"
                "### B"
+               "#t2"
                "## C"
                "# A" |]
         )
@@ -26,7 +88,16 @@ module DocSymbols =
             | _ -> failwith "Unexpected symbol type"
             |> Array.map (fun x -> x.Name)
 
-        Assert.Equal<string>([| "H1: E"; "H2: D"; "H3: B"; "H2: C"; "H1: A" |], symNames)
+        Assert.Equal<string>(
+            [| "H1: E"
+               "H2: D"
+               "H3: B"
+               "H2: C"
+               "H1: A"
+               "Tag: t1"
+               "Tag: t2" |],
+            symNames
+        )
 
     [<Fact>]
     let order_Hierarchy () =
@@ -45,4 +116,4 @@ module DocSymbols =
 
         syms |> Array.iter collect
 
-        Assert.Equal<string>([| "E"; "D"; "B"; "C"; "A" |], names)
+        Assert.Equal<string>([| "E"; "D"; "t1"; "B"; "t2"; "C"; "A" |], names)
